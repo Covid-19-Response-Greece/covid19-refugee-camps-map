b94580ff03339046c8f4ec042149895dda73dee9
NikosCha <NikosChaHellas@gmail.com>
2020-07-10 02:45:38 +0300

    init

---
+++ src/pages/info.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "components/Layout";
+import Container from "components/Container";
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <Container type="content" className="text-center">
+        <h1>Σχετικά με την πλατφόρμα Refugee Camps Greece - Covid19</h1>
+        <h1>Εθελοντική Δραση Covid19 Response Greece</h1>
+        <h1>Πηγές δεδομένων</h1>
+        <h1>Προτάσεις και Συζήτηση</h1>
+        <h1>Αδειοδότηση</h1>
+      </Container>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
