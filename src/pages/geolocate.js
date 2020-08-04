51021130ebdc01eedcc1db2989842d5e70e46df5
NikosCha <NikosChaHellas@gmail.com>
2020-08-04 20:13:32 +0300

    data api

--- src/pages/geolocate.js
+++ src/pages/geolocate.js
@@ -19,10 +19,10 @@ const SecondPage = () => {
   React.useEffect(() => {
     async function fetchData() {
       const response = await axios.get(
-        "https://api.jsonbin.io/b/5ee9d1a3ccc9877ac37d5cf0/7"
+        "https://covid-19-greece.herokuapp.com/refugee-camps"
       );
 
-      setdata(response.data);
+      setdata(response.data["refugee-camps"]);
     }
     fetchData();
   }, []);
